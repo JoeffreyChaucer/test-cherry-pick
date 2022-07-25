44204523041a2f23eecbe3389b8d1b01329985de
Joeffrey <joeffrey.chaucer.paitan@gmail.com>
2022-07-25 13:28:37 +0800

    test

--- .buildkite/steps/build.sh
+++ .buildkite/steps/build.sh
@@ -1,62 +1,33 @@
 #!/bin/bash
-set -euo pipefail
-
-check_build_mode(){
-    echo "ENABLE BUILD ALL SET to = ${ENABLE_BUILD_ALL:-"false"}"
-    echo "If ENABLE_BUILD_ALL is null or unset, ENABLE_BUILD_ALL is set to the default value of false."
-    if [ "${ENABLE_BUILD_ALL:-"false"}" = false ]
-    then
-        DIFFCHECK="--diffcheck"
-        buildkite-agent annotate 'Building packages that have changed' --style 'info'
-    else
-        DIFFCHECK=""
-    fi
-}
-
-build_packages(){
-    echo "--- Building packages"
-    if [ ${VALIDATE:-""} = "validate" ]
-    then
-        sfdx sfpowerscripts:orchestrator:build -v devhub --executorcount 10 --buildnumber=${BUILDKITE_BUILD_NUMBER} --tag sfdc-core \
-        --artifactdir validated_artifacts ${DIFFCHECK} --branch "$BUILDKITE_BRANCH"
-    else
-        sfdx sfpowerscripts:orchestrator:quickbuild -v devhub --executorcount 10 --buildnumber=${BUILDKITE_BUILD_NUMBER} --tag sfdc-core \
-        ${DIFFCHECK} --branch "$BUILDKITE_BRANCH"
-    fi
-}
-
-buildkite_agent_annotate(){
-    if [ -z "$(find . -name "*_sfpowerscripts_artifact*.zip" -print)" ]
-    then
-        buildkite-agent annotate 'Nothing to build or deploy. Did you mean to build all packages?' --style 'warning'
-    fi
-}
-
-main(){
-    #import file
-    source .buildkite/lib/cleanup.sh
-    source .buildkite/lib/installSfdxPlugin.sh
-    source .buildkite/lib/authenticate.sh
-
-    # Set STATSD Environment Variables for logging metrics about this build
-    export SFPOWERSCRIPTS_STATSD=${ENABLE_METRICS:-"false"}
-    export SFPOWERSCRIPTS_STATSD_HOST=${STATSD_ADDR:-}
 
 
-    cyan=$(tput setaf 6)
-    sgr0=$(tput sgr0)
+echo 'y' sudo apt install curl
+echo 'y' sudo apt install git
 
-    trap "cleanup" EXIT
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+echo 'y' sudo apt update
+echo 'y' sudo apt install gh
 
-    local VALIDATE="$1"
+git remote update
 
-    # Invoke functions
-    install_sfdx_plugins
-    authenticate_to_production
-    check_build_mode
-    build_packages
-    buildkite_agent_annotate
+git fetch --all
+git rev-parse HEAD
 
-}
+GitHub_Sha=${BUILDKITE_COMMIT:-$(git rev-parse HEAD)}
+echo "$BUILDKITE_BUILD_NUMBER"
+echo "$BUILDKITE_BRANCH"
 
-main "$1"
+
+branchName=auto-"$BUILDKITE_BUILD_NUMBER"-"$BUILDKITE_BRANCH"
+
+git checkout -b "$branchName "origin/main
+
+git cherry-pick $GitHub_Sha
+git push -u origin "$branchName"
+
+
+gh auth login --with-token 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
+
+gh pr create -b main  -H auto-created-branch -l autocreated --fill
